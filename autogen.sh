47e2f49ffc5c9300097ffb217074b0cac13f1e42
Eric Anholt <anholt@FreeBSD.org>
2006-05-22 23:51:39 -0700

    Replace autogen.sh with a more standard one from that does srcdir != builddir.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+ORIGDIR=`pwd`
+cd $srcdir
+
 aclocal || exit 1
 automake --foreign --add-missing || exit 1
 autoconf || exit 1
-./configure --enable-maintainer-mode "$@"
+
+cd $ORIGDIR || exit $?
+
+$srcdir/configure --enable-maintainer-mode "$@"
