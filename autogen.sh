37eac61e71a313df9927ca2a41ef49bda92fd9c6
Ian Romanick <ian.d.romanick@intel.com>
2009-01-20 12:04:46 -0800

    Make autogen.sh look like the autogen.sh in every other fd.o project
    Explicitly calling aclocal in autogen.sh circumvents the user's
    ACLOCAL environment setting and pretty much always does the wrong thing.

--- autogen.sh
+++ autogen.sh
@@ -6,9 +6,7 @@ test -z "$srcdir" && srcdir=.
 ORIGDIR=`pwd`
 cd $srcdir
 
-aclocal || exit 1
-automake --foreign --add-missing || exit 1
-autoconf || exit 1
+autoreconf -v --install || exit 1
 
 cd $ORIGDIR || exit $?
 
