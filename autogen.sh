c828b535a914fdf1da54cc521ebe6bf9078d80ba
Eric Anholt <eric@anholt.net>
2016-02-01 13:48:47 -0800

    autogen: Set a default prefix for patches if unset.
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- autogen.sh
+++ autogen.sh
@@ -11,3 +11,6 @@ autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
 $srcdir/configure --enable-maintainer-mode "$@"
+
+git config --local --get format.subjectPrefix ||
+    git config --local format.subjectPrefix "PATCH rendercheck"
